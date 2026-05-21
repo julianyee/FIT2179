e75efa49eee080c3c12aa47060bca0d358d2135c
julianyee <jyee0054@student.monash.edu>
2026-05-21 18:15:37 +0800

    Add files via upload

--- js/vega_embed.js
+++ js/vega_embed.js
@@ -2,5 +2,7 @@ vegaEmbed("#chart1", "charts/chart1_arrival_trend.vg.json", { actions: false }).
 vegaEmbed("#chart2", "charts/chart2_bump_origin_rank.vg.json", { actions: false }).catch(console.error);
 vegaEmbed("#chart3", "charts/chart3_dumbbell_country_change.vg.json", { actions: false }).catch(console.error);
 vegaEmbed("#chart4", "charts/chart4_world_origin_map.vg.json", { actions: false }).catch(console.error);
-vegaEmbed("#chart5", "charts/chart5_region_share_area.vg.json", { actions: false }).catch(console.error);
+vegaEmbed("#chart5", "charts/chart5_region_share_stacked_bar.vg.json", { actions: false }).catch(console.error);
 vegaEmbed("#chart6", "charts/chart6_monthly_arrival_heatmap.vg.json", { actions: false }).catch(console.error);
+vegaEmbed("#chart7", "charts/chart7_entry_state_lollipop.vg.json", { actions: false }).catch(console.error);
+vegaEmbed("#chart8", "charts/chart8_gender_composition.vg.json", { actions: false }).catch(console.error);
